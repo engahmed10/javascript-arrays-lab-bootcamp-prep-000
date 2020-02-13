9297fc7a2cb8787489ed50757f87794404378555
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 07:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var kittens = [] //define your array here
-
+//define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 // Add your functions and code here
+function destructivelyAppendKitten(name)
+{
+ kittens = kittens.concat(name)
+ return kittens
+}
+
+function destructivelyPrependKitten(name)
+{
+ kittens.unshift(name)
+ return kittens
+}
+destructivelyRemoveLastKitten()
+{
+ kittens.pop(name)
+ return kittens
+}
